58e8e93d9a750c6e1f9c97e296c7e551cf3534b6
神明 <shenming@shenmingMac.local>
2022-05-09 18:32:49 +0800

    头部导航构建

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React, { ReactNode } from 'react';
 import './App.css';
+import './assets/css/reset.css'
 import RouteConfig from './route/route';
+import MenuHeader from './components/menu_header';
 import './app.scss'
 
 const App = (): React.ReactElement<ReactNode> => {
   return (
     <div className="App">
-      <div className='header'>
-        头部
-      </div>
+      {/* 头部导航 */}
+      <MenuHeader/>
       <div className='content-box'>
         <div className='menu'>
           菜单
---
+++ src/components/icon_font.tsx
@@ -0,0 +1,6 @@
+import { createFromIconfontCN } from "@ant-design/icons";
+
+const IconFont: any = createFromIconfontCN({
+    scriptUrl: "https://at.alicdn.com/t/font_3205581_py0cd5oos9.js?spm=a313x.7781069.1998910419.53&file=font_3205581_py0cd5oos9.js",
+});
+export default IconFont;
