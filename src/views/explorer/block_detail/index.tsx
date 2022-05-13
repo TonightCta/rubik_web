42a4617ae8acd55e2508b59507dde7579ffc75d7
LyiZri <1296729980@qq.com>
2022-05-13 10:17:06 +0800

    test

--- src/views/explorer/block_detail/index.tsx
+++ src/views/explorer/block_detail/index.tsx
@@ -1,9 +1,40 @@
 import React,{ReactNode} from "react";
-
+import './index.scss'
 const BlockDetail = ():React.ReactElement<ReactNode>=>{
     return (
-        <div>
+        <div className="block-detail-bg">
+            <div className="block-detail">
+                    <div className="block-detail-box">
+                <table>
+                    <tr>
+                        <th></th>
+                        <th><span>hash
+                            </span> </th>
+                        <th><span>parent</span> </th>
+                        <th><span>extrinsics</span> </th>
+                        <th><span>state</span> </th>
+                        <th><span></span></th>
+                    </tr>
+                    <tr>
+                        <td><span>用户1</span> </td>
+                        <td><span>0xe08e634ede00a3474bed41886a</span> </td>
+                        <td><span>0x9a60fd115e87d6303df1074d4800</span></td>
+                        <td><span>0xe5ce99d9cebe7e46335be1b22</span></td>
+                        <td><span>0xe5ce99d9cebe7e46335be1b22</span></td>
+                        <td><span>View this externally</span> </td>
+                    </tr>
+                </table>
+                    </div>
+                    <div className="block-detail-box">
 
+                    </div>
+                    <div className="block-detail-box">
+
+                    </div>
+                    <div className="block-detail-box">
+
+                    </div>
+            </div>
         </div>
     )
 }
